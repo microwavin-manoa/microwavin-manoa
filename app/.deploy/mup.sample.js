4f7afe0a6317738724c127d3fa6a03172d4dc70b
Jiahui Liao <jiahuil@hawaii.edu>
2022-04-12 14:45:03 -1000

    Digital Ocean Deployed

--- app/.deploy/mup.sample.js
+++ app/.deploy/mup.sample.js
@@ -1,26 +1,26 @@
 module.exports = {
   servers: {
     one: {
-      host: '111.222.333.444',
+      host: '159.223.149.189',
       username: 'root',
-      password: 'changeme'
+      password: 'Microwavin123Manoa'
     }
   },
   app: {
     // if you edit the app 'name' field, be sure to run 'mup stop' if the app is already running.
     // otherwise you will have two apps deployed at once, with unpredictable results.
-    name: 'meteor-application-template-react',
+    name: 'Microwavin-Manoa',
     path: '../',
     servers: { one: {}, },
     buildOptions: { serverOnly: true },
     env: {
-      ROOT_URL: 'http://111.222.333.444',
+      ROOT_URL: 'http://159.223.149.189',
       MONGO_URL: 'mongodb://mongodb/meteor',
       MONGO_OPLOG_URL: 'mongodb://mongodb/local',
     },
     docker: { image: 'zodern/meteor:latest' },
     enableUploadProgressBar: true
   },
-  mongo: { version: '3.4.1', servers: { one: {} }
+  mongo: { version: '5.0.5', servers: { one: {} }
   },
 };
