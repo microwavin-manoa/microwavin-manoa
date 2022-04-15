5f7c490cfaa96a7c4ff17086f026d964ac061279
carolwong492 <carolwong492@gmail.com>
2022-04-15 00:37:02 -1000

    Created default tags collection

---
+++ app/imports/api/tag/Tags.js
@@ -0,0 +1,26 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/**
+ * The StuffsCollection. It encapsulates state and variable values for stuff.
+ */
+class TagsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'TagsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      name: String,
+    }, { tracker: Tracker });
+    // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+export const Tags = new TagsCollection();
