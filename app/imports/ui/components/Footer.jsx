dba1e002a9941bbcb81a53f0a1ca0f8a8d3ade28
Jiahui Liao <jiahuil@hawaii.edu>
2022-04-19 21:01:18 -1000

    landing page revamp finished

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
-    const divStyle = { paddingTop: '15px' };
+    const divStyle = { paddingTop: '15px', position: 'relative', fontSize: '16px', color: '#f5f0e6', minHeight: '25vh' };
+    const footerStyle = { position: 'absolute', width: '100%', backgroundColor: '#ceb793', bottom: '0' };
     return (
-      <footer>
-        <div style={divStyle} className="ui center aligned container">
-          <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822 <br />
-          <a href="https://microwavin-manoa.github.io/">GitHub Project Page</a>
-        </div>
-      </footer>
+      <div style={divStyle}>
+        <footer style={footerStyle}>
+          <Container className="center aligned">
+            Department of Information and Computer Sciences <br />
+            University of Hawaii<br />
+            Honolulu, HI 96822 <br />
+            <a href="https://microwavin-manoa.github.io/">GitHub Project Page</a>
+          </Container>
+        </footer>
+      </div>
     );
   }
 }
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,27 +1,43 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid, Segment, Container } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
+    const hstyle = { textAlign: 'center', color: 'white', fontSize: '50px', padding: '50px' };
+    const fontSize = { fontSize: '17px' };
     return (
-      <div id='landing-page' container>
-        <Grid verticalAlign='middle' textAlign='center'>
-          <Grid.Column width={5}>
-            <Image size='huge' rounded src="/images/food.jpg" alt="Picture of food on table"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Microwavin Manoa</h1>
-            <p><b>Are you a busy, broke college student with no time, no cooking skills, and no energy to cook?</b></p>
-            <p>Microwavin Manoa has got you covered! Microwavin Manoa creates a way for students
-              (on-campus or off) to learn and share recipes that can be made using minimal kitchen
-              facilities, can be made out of ingredients that are available within walking distance of
-              UH, and can be filtered via dietary restrictions.</p>
-            <p>Each recipe will come with detailed instructions, as well as locations to buy the
-              ingredients and how much it will cost overall.</p>
-          </Grid.Column>
-        </Grid>
+      <div id="landing-page">
+        <Segment id="landingimage">
+          <h1 style={hstyle}>Microwavin Manoa</h1>
+          <h2 style={hstyle}>Are you a busy, broke college student with no time, no cooking skills, and no energy to cook?</h2>
+        </Segment>
+        <div>
+          <Container>
+            <Grid columns='three' divided centered >
+              <Grid.Row>
+                <Grid.Column style={fontSize}>
+                  <h1>About Us</h1>
+                  <p>Microwavin Manoa creates a way for students
+                    (on-campus or off) to learn and share recipes that can be made using minimal kitchen
+                    facilities, can be made out of ingredients that are available within walking distance of
+                    UH, and can be filtered via dietary restrictions.</p>
+                </Grid.Column>
+                <Grid.Column style={fontSize}>
+                  <h1>Learn and Share Recipes</h1>
+                  <p>Users can upload and search through recipes. Filter or search by tags to find a recipe you like.
+                    Each recipe will come with detailed instructions, as well as locations to buy the
+                    ingredients and how much it will cost overall. </p>
+                </Grid.Column>
+                <Grid.Column style={fontSize}>
+                  <h1>Get to Microwavin</h1>
+                  <p>After finding a recipe, users are able to view vendors nearby that sell that recipe&apos;s ingredients, as well as the price of each ingredient.
+                    These recipes can be made with simple equipment that most have on-hand.</p>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </div>
       </div>
     );
   }
