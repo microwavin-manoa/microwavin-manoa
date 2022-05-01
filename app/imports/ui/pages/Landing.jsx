4eef2fdef59b84f71d0743684ff37ed0d0d2a881
KaileeHung <97822968+KaileeHung@users.noreply.github.com>
2022-04-30 16:48:20 -1000

    update landing page beta

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Segment, Container } from 'semantic-ui-react';
+import { Grid, Segment, Container, Header, Image } from 'semantic-ui-react';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
 import { Roles } from 'meteor/alanning:roles';
@@ -9,49 +9,77 @@ import { withRouter } from 'react-router-dom';
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
-    const hstyle = { textAlign: 'center', color: 'white', fontSize: '50px', padding: '50px' };
-    const fontSize = { fontSize: '17px' };
     return (
       <div id="landing-page">
-        <Segment id="landingimage">
-          <h1 style={hstyle}>Microwavin Manoa</h1>
-          <h2 style={hstyle}>Are you a busy, broke college student with no time, no cooking skills, and no energy to cook?</h2>
-        </Segment>
-        <div>
-          <Container>
-            <Grid columns='three' divided centered >
-              <Grid.Row>
-                <Grid.Column style={fontSize}>
-                  <h1>About Us</h1>
-                  <p>Microwavin Manoa creates a way for students
-                    (on-campus or off) to learn and share recipes that can be made using minimal kitchen
-                    facilities, can be made out of ingredients that are available within walking distance of
-                    UH, and can be filtered via dietary restrictions.</p>
-                </Grid.Column>
-                <Grid.Column style={fontSize}>
-                  <h1>Learn and Share Recipes</h1>
-                  <p>Users can upload and search through recipes. Filter or search by tags to find a recipe you like.
-                    Each recipe will come with detailed instructions, as well as locations to buy the
-                    ingredients and how much it will cost overall. </p>
-                </Grid.Column>
-                {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-                  <Grid.Column style={fontSize}>
-                    <h1>Edit Recipes and Vendors</h1>
-                    <p>Admin are able to edit and delete recipes, as well as edit vendor profiles from a list of all
-                      available recipes and vendors. Admin are able to view the owner of each recipe.</p>
-                  </Grid.Column>
-                ) : ''}
-                {!Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-                  <Grid.Column style={fontSize}>
-                    <h1>Get to Microwavin</h1>
-                    <p>After finding a recipe, users are able to view vendors nearby that sell that recipe&apos;s ingredients, as well as the price of each ingredient.
-                      These recipes can be made with simple equipment that most have on-hand.</p>
-                  </Grid.Column>
-                ) : ''}
-              </Grid.Row>
-            </Grid>
-          </Container>
+        <div id='landing-image'>
+          <Grid container centered stackable columns={2} verticalAlign='middle' className='landing-content1'>
+            <Grid.Column textAlign='center'>
+              <Header as='h1' style={{ fontSize: '70px' }}>Microwavin<br/>Manoa</Header>
+            </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Header as='h1'>About Us</Header>
+              <Header as='h2'>
+                Create and discover recipes made tailor-made for UH Manoa students (on-campus or off)! We focus on recipes that can be made using minimal kitchen facilities (like a microwave!), and ingredients available within walking distance of UH
+              </Header>
+            </Grid.Column>
+          </Grid>
         </div>
+        <div className='green-background'></div>
+        <div className='white-background'>
+          <Grid container centered stackable columns={2} verticalAlign='middle' className='landing-content2'>
+            <Grid.Column textAlign='center'>
+              <Image src='images/register.png'/>
+            </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Header as='h2' style={{ fontSize: '40px' }}>Create an account<br/>and log in</Header>
+              <br/>
+              <Image src='images/leafy-divide.png' size='medium' floated='right'/>
+            </Grid.Column>
+          </Grid>
+        </div>
+        <div className='green-background'></div>
+        <div className='white-background' style={{ height: '460px', backgroundColor: '#f5f0e6' }}>
+          <Grid container centered stackable columns='equal' verticalAlign='middle' className='landing-content2'>
+            <Grid.Row>
+              <Header as='h2' style={{ paddingTop: '10px' }}>Create recipes, view and edit your own recipes</Header>
+            </Grid.Row>
+            <Grid.Column textAlign='center'>
+              <Image src='images/register.png'/>
+            </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Image src='images/register.png'/>
+            </Grid.Column>
+          </Grid>
+        </div>
+        <div className='green-background'></div>
+        <div id='landing-image2'>
+          <Grid container centered stackable columns={2} verticalAlign='middle' className='landing-content3'>
+            <Grid.Column textAlign='center'>
+              <Header as='h2' style={{ fontSize: '40px' }}>Search and<br/>filter recipes</Header>
+            </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Image src='images/register.png'/>
+            </Grid.Column>
+          </Grid>
+        </div>
+        <div className='green-background'></div>
+        <div className='white-background' style={{ height: '500px', backgroundColor: '#f5f0e6' }}>
+          <Grid container centered stackable columns='equal' verticalAlign='middle' className='landing-content2'>
+            <Grid.Row>
+              <Header as='h2' style={{ paddingTop: '10px' }}>View recipes and vendor information</Header>
+            </Grid.Row>
+            <Grid.Row>
+              <Header as='h2' style={{ fontSize: '15px' }}>Update price data and add new ingredients</Header>
+            </Grid.Row>
+            <Grid.Column textAlign='center'>
+              <Image src='images/register.png'/>
+            </Grid.Column>
+            <Grid.Column textAlign='center'>
+              <Image src='images/register.png'/>
+            </Grid.Column>
+          </Grid>
+        </div>
+        <div className='green-background'></div>
       </div>
     );
   }
